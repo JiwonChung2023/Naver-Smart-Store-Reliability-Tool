6844b2057e9f59df181b707bf2d1ff26fa906adf
Jiwon Chung <0000againism@korea.ac.kr>
2023-04-11 17:47:01 +0900

    new file:   "csvs/\354\243\274\354\213\235\355\232\214\354\202\254 \354\232\260\352\263\240\353\236\230\353\271\2273+1 \354\232\260\352\263\240\353\236\230\353\271\227 \353\202\250\354\233\220 \354\260\271\354\214\200\352\271\200\353\266\200\352\260\201 \354\210\230\354\240\234 \353\266\200\352\260\201 50g HACCPcomdata.csv" 	new file:   "csvs/\354\243\274\354\213\235\355\232\214\354\202\254 \354\232\260\352\263\240\353\236\230\353\271\227StoreInfo.csv" 	new file:   "csvs/\354\243\274\354\213\235\355\232\214\354\202\254 \354\232\260\352\263\240\353\236\230\353\271\227\354\232\260\352\263\240\353\236\230\353\271\227 \353\217\274\354\247\200\352\260\220\354\236\220\354\260\250 \354\236\221\353\221\220\354\275\251\354\260\250 \353\221\245\352\265\264\353\240\210\354\260\250 \354\227\254\354\243\274\354\260\250 \355\230\270\353\260\225\355\214\245\354\260\250 \353\217\204\353\235\274\354\247\200\354\260\250 \353\271\204\355\212\270\354\260\250 \352\262\200\354\235\200\354\275\251 \353\252\250\352\263\274\354\260\250comdata.csv" 	new file:   "csvs/\354\243\274\354\213\235\355\232\214\354\202\254 \354\232\260\352\263\240\353\236\230\353\271\227\354\232\260\352\263\240\353\236\230\353\271\227 \355\226\207 \354\203\235\354\225\204\353\252\254\353\223\234 \353\213\271\354\235\274\353\263\266\354\235\200 \352\265\254\354\232\264\354\225\204\353\252\254\353\223\234 \353\263\266\354\235\214 HACCPcomdata.csv" 	new file:   "csvs/\354\243\274\354\213\235\355\232\214\354\202\254 \354\232\260\352\263\240\353\236\230\353\271\227\354\232\260\352\263\240\353\236\230\353\271\227 \355\227\210\353\270\214\355\213\260 50\355\213\260\353\260\261 \354\272\220\353\252\250\353\247\210\354\235\274\354\260\250 \355\236\210\353\271\204\354\212\244\354\273\244\354\212\244 \355\216\230\355\215\274\353\257\274\355\212\270 \354\236\220\354\212\244\353\257\274 \353\241\234\354\246\210\353\247\210\353\246\254 \352\263\274\354\235\274 \354\225\240\355\224\214 \353\270\224\353\240\214\353\224\251\355\213\260comdata.csv" 	new file:   "csvs/\354\243\274\354\213\235\355\232\214\354\202\254 \354\232\260\352\263\240\353\236\230\353\271\227\354\232\260\352\263\240\353\236\230\353\271\227 \355\231\215\354\260\250 \354\226\274\352\267\270\353\240\210\354\235\264 \354\225\204\354\214\210 \353\213\244\354\246\220\353\247\201 \354\236\211\352\270\200\353\246\254\354\211\254\353\270\224\353\236\231\355\215\274\354\212\244\355\212\270 \353\270\224\353\240\214\353\224\251\355\213\260 1.5g 50\355\213\260\353\260\261comdata.csv" 	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# %%
-# 라이브러리 가져오기
-
+# 라이브러리를 가져오기
 import sqlite3
 import numpy as np
 import pandas as pd
@@ -11,24 +9,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-# %%
-# 웹드라이버 가져오기
+
+# 웹드라이버를 가져오기
 cdriver='./driver/chromedriver.exe'
 driver=webdriver.Chrome(cdriver)
 driver.set_window_position(0,50)
 driver.set_window_size(800, 1300)
-# 디비 정의 및 함수 정의하기
-# %%
+
+# 디비를 정의하기
 dfile='./db/suspiciousStores0410.db'
-# "select" 구문
+# 함수를 정의하기
 def sqlPrs(sql='',d=[],opt=1):
     with sqlite3.connect(dfile) as conn:
         cur=conn.cursor()
-        # select 문의 경우 실행 후 데이터 반환이 필요하다!
         if opt==1:
             res=cur.execute(sql).fetchall()
         elif opt==2:
-            # insert: insert into mvreview (rid,title,href,point,user,rday,content) values(?,?,?,?,?,?,?)
             res=cur.execute(sql,d)
         else:
             cur.execute(sql)
@@ -37,24 +33,24 @@ def sqlPrs(sql='',d=[],opt=1):
     return(res)
 def clickIt(ssel,drv=driver):
     drv.find_element(by=By.CSS_SELECTOR,value=ssel).click()
-def goScroll(level=0):
-    #dheight=driver.execute_script('return document.documentElement.scrollHeight')
-    dheight=driver.execute_script('window.scrollTo(0,{})'.format(level))
-# %%
+def goScroll():
+    dheight=driver.execute_script('return document.documentElement.scrollHeight')
+    dheight=driver.execute_script('window.scrollTo(0,{})'.format(dheight))
+
+# db에 있는 스토어 리스트를 가져오기
 sql='select link from STLC'
 res=sqlPrs(sql)
-#print(res[0][0])
-# %%
+
+# 스토어 리스트의 길이를 가져오기
 sellerLength=len(res)
-j=0
-for i in range(sellerLength):
-    j+=1
+li1=[]
+for i in range(65,66):
     sellerURL=res[i][0]
     # 판매자 정보에서 정보를 얻기
     sellerInfo='/profile'
     url=sellerURL+sellerInfo
     driver.get(url)
-    time.sleep(2) # 딜레이를 통해 웹이 로딩되는 시간을 기다려줌
+    time.sleep(2) 
     # 정보 가져오기
     stNameSel='#content > div > div._3MuEQCqxSb > div._2i91yA8LnF > div.oSdeQo13Wd > div > div:nth-child(1) > div:nth-child(1) > div._2PXb_kpdRh'
     stName=driver.find_element(by=By.CSS_SELECTOR,value=stNameSel).text
@@ -68,28 +64,105 @@ for i in range(sellerLength):
     male=txt.split('%')[0][-2:]+'%'
     female=txt.split('%')[1]+'%'
     attSel='#header > div > div._1Y0GXNu6q8 > div._3KDc7jvaa-'
-    att=driver.find_element(by=By.CSS_SELECTOR,value=attSel).text.split('\n')[0].split('수 ')[1]
-    print(f'상호명: {stName}\n사업자등록번호: {busNumber}\n사업장 소재지: {busAddress}\n남성 비율: {male}\n여성 비율: {female}\n관심고객수: {att}')
-    
+    att=driver.find_element(by=By.CSS_SELECTOR,value=attSel).text.split('\n')[0].split('수 ')[1]    
     busCheck='http://apis.data.go.kr/1130000/MllBsService/getMllBsInfo?serviceKey=55yUz9MpoHrSz%2B8C53zU9sLbqwDB2Rt9EIvBt9J6qk03ke9IexQYqBb50XkfXsR6H6kkByxLJkx7HJdYczbffA%3D%3D&pageNo=1&numOfRows=10&resultType=xml&bizrno='
-    #driver.get(busCheck+busNumber)
     req=requests.get(busCheck+busNumber)
     html=req.text
     src2=bsp(html,'html.parser')
+    busok=''
+        
     try:
         isok=src2.select('mngstatenm')[0].text
         if(isok=='정상영업'):
-            print('True Business')
-            print('-'*30)
+            busok='True Business'
+            li1.append([stName,busNumber,busAddress,male,female,att,busok])
         else:
-            print(isok)
-            print('-'*30)
+            busok=isok
+            li1.append([stName,busNumber,busAddress,male,female,att,busok])
     except:
-        print('사업장번호를 입력하지 않았습니다.')
-        print('-'*30)
-    if(j==10):
+        busok='사업장번호를 입력하지 않았습니다.'
+        li1.append([stName,busNumber,busAddress,male,female,att,busok])
+
+    df1=pd.DataFrame(li1,columns=['상호명','사업자등록번호','사업장 소재지','남성비율','여성비율','관심고객수','정상영업여부'])
+    df1.to_csv(f'./csvs/{stName}StoreInfo.csv',encoding='utf-8-sig')
+
+satis='/category/ALL?cp=1'
+driver.get(sellerURL+satis)
+time.sleep(2)
+clickIt('#CategoryProducts > div._3y-z4lfyMn > div.Ii5tIcy54E > div > div._19Yfb5AYEX > ul > li:nth-child(5) > button')
+time.sleep(2)
+clickIt('#CategoryProducts > div._3y-z4lfyMn > div.Ii5tIcy54E > div > div.IwEWcMcLlb > div._3SdQ5ltYC7 > div > ul > li:nth-child(3) > button')
+time.sleep(1)
+li2=[]
+jwlist2=[]
+
+for j in range(1,6):
+    try:
+        m=2
+        rstar=[]
+        rdate=[]
+        roption=[]
+        rcomm=[]
+        wordBox=''
+        proSel='#CategoryProducts > ul > li:nth-child({}) > div > a'
+        driver.find_element(by=By.CSS_SELECTOR,value=proSel.format(j)).click()
+        time.sleep(2)
+
+        proNameSel='#content > div > div._2-I30XS1lA > div._2QCa6wHHPy > fieldset > div._3k440DUKzy > div._1eddO7u4UC > h3'
+        proName=driver.find_element(by=By.CSS_SELECTOR,value=proNameSel).text
+        revSel='#content > div > div.z7cS6-TO7X > div._27jmWaPaKy > ul > li:nth-child(2) > a'
+        driver.find_element(by=By.CSS_SELECTOR,value=revSel).click()
+        time.sleep(2)
+        totalSel='#content > div > div._2-I30XS1lA > div.-g-2PI3RtF > div.NFNlCQC2mv'
+        revinfo=driver.find_element(by=By.CSS_SELECTOR,value=totalSel).text.split('\n')[0]
+        li2.append([stName,proName,revinfo])
+        #jwlist=scrapePerItem()
+        lowRevSel='#REVIEW > div > div._180GG7_7yx > div._2PqWvCMC3e > div._3Tobq9fjVh > ul > li:nth-child(4) > a'
+        clickIt(lowRevSel)
+        time.sleep(1)
+        sel = '._1XNnRviOK8'
+        revs = driver.find_elements(by=By.CSS_SELECTOR,value=sel)
+        listSel='#REVIEW > div > div._180GG7_7yx > div.cv6id6JEkg > div > div > a:nth-child({})'
+        realjwlist=[]
+        while(m!=7):
+            try:
+                driver.find_element(by=By.CSS_SELECTOR,value=listSel.format(m)).click()
+                time.sleep(2)
+                ####
+                print('.',end='')
+                ####
+                #for l in listSel
+
+                for r in revs:
+                    rlist=r.text.split('\n')
+                    #print(rlist)
+                    rstar.append(rlist[1])
+                    rdate.append(rlist[3])
+                    rest=rlist[5:]
+                    if ('더보기' in rest):
+                        rest.remove('더보기')
+                    if ('이미지 펼쳐보기' in rest):
+                        rest.remove('이미지 펼쳐보기')
+                    if (':' in rest[0]):
+                        roption.append(rest[0])
+                        rest=rest[1:]
+                    if (len(rest)==1):
+                        rcomm.append(rest[-1])
+                    else:
+                        wordBox=''
+                        for n in rest:
+                            wordBox+=n
+                            wordBox+=' '
+                        rcomm.append(wordBox)
+                    jwlist=list(zip(rdate,rstar,roption,rcomm))
+                    realjwlist+=jwlist
+                m+=1
+            except:
+                m+=1
+
+        df3=pd.DataFrame(realjwlist,columns=['기입날짜','별점','옵션', '후기'])
+        df3.to_csv(f'./csvs/{stName}{proName}comdata.csv',encoding='utf-8-sig')
+        driver.back()
+        time.sleep(2)
+    except:
         break
-
-
-
-# %%
