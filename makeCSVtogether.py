bc29c082fc95d2efe7e6888dc5bf91a2d6e427e6
Jiwon2 <Jiwon2@naver.com>
2023-04-23 11:28:50 +0900

    라벨링 다시 요망

--- makeCSVtogether.py
+++ makeCSVtogether.py
@@ -31,20 +31,20 @@ merged_df.to_csv('./csvs/myHappyReviews.csv', encoding='utf-8-sig')
 
 #%%
 ###
-# 파일명 리스트 생성
-file_names = ['realTrain.csv','myHappyReviews.csv','myUnhappyReviews.csv']
+# # 파일명 리스트 생성
+# file_names = ['realTrain.csv','myHappyReviews.csv','myUnhappyReviews.csv']
 
-# 빈 DataFrame 생성
-merged_df = pd.DataFrame()
+# # 빈 DataFrame 생성
+# merged_df = pd.DataFrame()
 
-# 파일 읽어와서 DataFrame에 병합
-for file_name in file_names:
-    df = pd.read_csv('./csvs/'+file_name)
-    merged_df = pd.concat([merged_df, df])
+# # 파일 읽어와서 DataFrame에 병합
+# for file_name in file_names:
+#     df = pd.read_csv('./csvs/'+file_name)
+#     merged_df = pd.concat([merged_df, df])
 
-# 결과를 realTrain.csv 파일로 저장
-merged_df.to_csv('./csvs/realTrain.csv', encoding='utf-8-sig')
+# # 결과를 realTrain.csv 파일로 저장
+# merged_df.to_csv('./csvs/realTrain.csv', encoding='utf-8-sig')
 
 
 
-# %%
+# # %%
