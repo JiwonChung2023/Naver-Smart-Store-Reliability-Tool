2dd9c4a51eb3561f0440c5c3de540b153332d37f
JW PC in Asia <aaaa@naver.com>
2023-04-25 16:30:33 +0900

    장고 프레임워크 넣었음

---
+++ polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+# 장고 모델을 상속하여 새 모델 파일을 생성 -> DB의 테이블
+class Quest(models.Model):
+    qtxt=models.CharField(max_length=200)
+    qdate=models.DateTimeField('date published')
+
+class Choice(models.Model):
+    quest=models.ForeignKey(Quest,on_delete=models.CASCADE)
+    choice_txt=models.CharField(max_length=150)
+    votes=models.IntegerField(default=0)
+    vdate=models.DateTimeField('date published',default=datetime.now)
